67296d92225c40c958e729d7dea0e600e64afb35
Charles Lyu <shift16384@gmail.com>
2022-01-10 02:16:12 -0500

    Added list of cosmetic forms

--- params.py
+++ params.py
@@ -17,9 +17,56 @@ SPECIAL_BASE_DISPLAY_NAMES = {  # Default: Replace _ with space (Tapu Koko)
     'Porygon_Z': 'Porygon-Z',
     'FARFETCHD': 'Farfetch\'d',
     'SIRFETCHD': 'Sirfetch\'d',
+    'FLABEBE': 'Flabébé',
 }
-SPECIAL_FORM_DISPLAY_NAMES = {}
+SPECIAL_FORM_DISPLAY_NAMES = {
+    'A': 'Armored',
+}
 
-CAN_EVOLVE_FORCED_TRUE = [  # Species that have special evolution requirements don't show up in Pokebattler's GM (but do )
-
+COSMETIC_FORMS_UNIVERSAL = [  # Any Pokemon with these forms only have cosmetic changes
+                              # (e.g. should not be considered a different raid boss)
+    'FALL_2019', 'COPY_2019', 'COSTUME_2020', 'ADVENTURE_HAT_2020', 'WINTER_2020',
+    # Note Pikachu Costume 2020 is Flying Pikachu with the move Fly, but it's the same as FLYING_5TH_ANNIV
+    # VS_2019 is Pikachu Libre
+    '2020', '2021', '2022'
 ]
+COSMETIC_FORMS_PER_POKEMON = {  # These specific Pokemon have these cosmetic forms (i.e. same as their normal form)
+    'PIKACHU': ['KARIYUSHI'],
+    'SHELLOS': ['WEST_SEA', 'EAST_SEA'],
+    'GASTRODON': ['WEST_SEA', 'EAST_SEA'],
+    'BASCULIN': ['RED_STRIPED', 'BLUE_STRIPED'],
+    'KELDEO': ['ORDINARY', 'RESOLUTE'],
+    'DEERLING': ['SPRING', 'SUMMER', 'AUTUMN', 'WINTER'],
+    'SAWSBUCK': ['SPRING', 'SUMMER', 'AUTUMN', 'WINTER'],
+    'FRILLISH': ['FEMALE'],
+    'JELLICENT': ['FEMALE'],
+    'FURFROU': ['NATURAL', 'HEART', 'STAR', 'DIAMOND', 'DEBUTANTE', 'MATRON', 'DANDY', 'LA_REINE', 'KABUKI', 'PHARAOH'],
+    'SINISTEA': ['PHONY', 'ANTIQUE'],
+    'POLTEAGEIST': ['PHONY', 'ANTIQUE'],
+
+    # The following Pokemon have non-cosmetic forms, but one of them is identical to the "regular" form
+    # (the one with no forms listed, which also appears in both Pokebattler and GM data),
+    # hence this form is also considered cosmetic
+    'BURMY': ['PLANT'],
+    'WORMADAM': ['PLANT'],
+    'SHAYMIN': ['LAND'],
+    'CHERRIM': ['OVERCAST'],
+    'DARMANITAN': ['STANDARD'],  # Galarian has no "regular" form, only Standard and Zen
+    'TORNADUS': ['INCARNATE'],
+    'THUNDURUS': ['INCARNATE'],
+    'LANDORUS': ['INCARNATE'],
+    'MELOETTA': ['ARIA'],
+    'EISCUE': ['ICE'],
+    'INDEEDEE': ['MALE'],
+    'MORPEKO': ['FULL_BELLY'],  # Considering Hangry as non-cosmetic since Aura Wheel could be added one day
+    'ZACIAN': ['HERO'],
+    'ZAMAZENTA': ['HERO'],
+    'PUMPKABOO': ['SMALL'],
+    'GOURGEIST': ['SMALL'],
+    'HOOPA': ['CONFINED'],
+}
+# TODO: When using any Pokemon above, make sure only their regular forms are considered
+#  (and not considered again if one of the cosmetic forms already is)
+IGNORED_FORMS = {  # Forms that should not exist
+    'URSHIFU': [''],  # Current GM has Urshifu "regular" form as mono Fighting
+}
